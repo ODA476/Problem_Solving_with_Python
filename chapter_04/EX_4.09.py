ee1d89a8fb985caa6b9364daee6fd797f8bfa3e4
ODA476 <omk4762002@gmail.com>
2023-08-08 15:33:20 +0300

    chapter_4 EX_4.09, EX_4.10

---
+++ chapter_04/EX_4.09.py
@@ -0,0 +1,21 @@
+"""
+*4.9 (Financial: compare costs) Suppose you shop for rice and find it in two differentsized packages. You would like to write a program to compare the costs of the
+packages. The program prompts the user to enter the weight and price of each
+package and then displays the one with the better price.
+"""
+
+# Enter weight and price for package 1
+weight1, price1 = eval(input('Enter weight and price for package 1: ').strip())
+
+# Enter weight and price for package 2
+weight2, price2 = eval(input('Enter weight and price for package 2: ').strip())
+
+# compute one unit of weight per its price
+weight_per_price_1 = weight1 / price1
+weight_per_price_2 = weight2 / price2
+
+# choose the better, the better has less cost than other
+if weight_per_price_1 < weight_per_price_2:
+    print('Package 1 has the better price.')
+else:
+    print('Package 2 has the better price.')
